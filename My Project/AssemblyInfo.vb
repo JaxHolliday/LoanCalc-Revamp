161b2dfa0c001e4c98c8e8f00f62d493695aa57f
JaxHolliday <JaxHolliday@users.noreply.github.com>
2021-01-18 10:20:24 -0500

    Main Push

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("HollidayLoanCalc")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HollidayLoanCalc")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("edef934f-4564-4069-9b23-f5e2255255e8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ js/MortCalc.js
@@ -0,0 +1,139 @@
+﻿var dataEntries = []; // Prin Payments per month (For Bar Graph)
+
+function calculate() {
+    //Step 1: Getting the data
+    let bal = Number(document.getElementById("balance").value); // Input 1 is total balance input
+    let term = Number(document.getElementById("term").value); // Input 2 is the monthly term input
+    let rate = Number(document.getElementById("rate").value);  // Input 3 is the interest rate input
+
+
+    //Step 2: Run Collected Data
+    // Total Monthly Payment = (amount loaned) * (rate/1200) / (1 – (1 + rate/1200)^(-Number of Months) )
+    let totMonPay = (bal) * (rate / 1200) / (1 - Math.pow((1 + rate / 1200), -term)); 
+    //rem balance = balance after each monthly pay
+    let remBal = bal;     
+    //Interest Payment = Previous Remaining Balance * rate/1200
+    let intPay = remBal * rate / 1200;      
+    // Principal Payment = Total Monthly Payment - Interest Payment
+    let prinPay = totMonPay - intPay;     
+    //Remaining Balance = Previous Remaining Balance - principal payments
+    let totIntPay = 0;                                               
+    let accuPay = 0;
+
+    //Step 3: Result / Outputting to table
+    // Base format of the Table
+    let template = `<tr>
+        <th>Month</th>
+        <th>Payment</th>
+        <th>Principal</th>
+        <th>Interest</th>
+        <th>Total Interest</th>
+        <th>Balance</th>        
+    </tr>`;
+
+
+    //Loop will dynamically fill the table based on how many months the interest is
+    // i = starting month
+
+    for (let i = 1; i <= term; i++) {
+        //total interest will gradually increase as monthly payments are made
+        totIntPay += intPay;   
+        //Principal will increase since interest amount owed goes down 
+        prinPay = totMonPay - intPay; 
+        //Remaining balance will slowly decrease as payments are made
+        remBal = remBal - prinPay; 
+        //As loop continues, it gradually decreases as more payments are made
+        intPay = remBal * rate / 1200;              
+        accuPay += totMonPay;
+
+        //(Bar Graph) checks for 1st 6 months and pushes principal pay into data entries variable
+        if (i <= 6) {                               
+            dataEntries.push(accuPay);
+        }
+
+
+        //How result will be formatted as loop continues 
+        template += `<tr>
+        <td>${i}</td>
+        <td>${totMonPay.toFixed(2)}</td>
+        <td>${prinPay.toFixed(2)}</td>
+        <td>${intPay.toFixed(2)}</td>
+        <td>${totIntPay.toFixed(2)}</td>
+        <td>${remBal.toFixed(2)}</td>
+    </tr>`
+    };
+
+    //Puts into HTML
+    document.getElementById("result").innerHTML = template;
+    document.getElementById("termMon").innerHTML = `${term} months`
+    document.getElementById("totBal").innerHTML = `$${bal}`
+    document.getElementById("monPay").innerHTML = `$${totMonPay.toFixed(2)}`;
+    document.getElementById("intRate").innerHTML = `${rate}%`
+
+    prinGraph();
+
+};
+
+// function prevents input of all letters into input field
+function isNumber(evt) {                                     //parameter of event
+    evt = (evt) ? evt : window.event;                        //evt variable = event of the key press
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 46) || (charCode > 46 && (charCode < 48) || charCode > 57)) {
+        return false;
+    }
+    return true;
+};
+
+//Prints graph showing principal paid based on the first 6 months 
+function prinGraph() {
+    document.getElementById("chart").innerHTML = ``;
+    var options = {
+        chart: {
+            type: 'bar'
+        },
+        series: [{
+            name: 'Accumulated Payments',
+            data: [dataEntries[0].toFixed(2), dataEntries[1].toFixed(2), dataEntries[2].toFixed(2), dataEntries[3].toFixed(2), dataEntries[4].toFixed(2), dataEntries[5].toFixed(2)]
+        }],
+        xaxis: {
+            categories: ["Month 1", "Month 2", "Month 3", "Month 4", "Month 5", "Month 6"],            
+        }
+    }
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+    dataEntries = [];                   // Resets array to empty 
+};
+
+//displays empty chart at the launch of the page 
+window.onload = function defaultGraph() {
+    document.getElementById("chart").innerHTML = `<br><br><br><br><br><br><br><br><br><br><br><br><br>`;
+}
+
+
+/////////////////////////////////////////////////////////////////////
+//                         SHYANN EDITS                            //
+/////////////////////////////////////////////////////////////////////
+
+
+// Button Event
+document.getElementById('calc').addEventListener('click', function () {
+
+    let verifyBal = Number(document.getElementById('balance').value);
+    let verifyTerm = Number(document.getElementById('term').value);
+    let verifyRate = Number(document.getElementById('rate').value);
+
+
+    if (verifyBal == "" || verifyTerm == "" || verifyRate == "" ) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please fill out all selections before using the calculator.',
+        })
+    }
+    else {
+        calculate();
+        //taxFunction();
+    }
+});
+
